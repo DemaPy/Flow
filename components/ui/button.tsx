ab98476142462c03d5d136536bb8f0dac76dfefd
DemaPy <benderdema@gmail.com>
2024-11-12 11:03:50 +0100

    404 page has been created

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,8 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         sidebarItem: "gap-2 !justify-start hover:bg-accent hover:bg-primary/80 hover:text-primary-foreground",
-        sidebarActiveItem: "gap-2 !justify-start hover:bg-primary/90 bg-primary/80 text-primary-foreground"
+        sidebarActiveItem: "gap-2 !justify-start hover:bg-primary/90 bg-primary/80 text-primary-foreground",
+        icon: "flex items-center justify-center px-4 py-2 bg-primary text-white rounded-md hover:bg-primary/80 transition-colors"
       },
       size: {
         default: "h-10 px-4 py-2",
