b2c69af5bd96f722264b9dce9ca28f6bf9752a28
DemaPy <benderdema@gmail.com>
2024-11-22 21:35:36 +0100

    cron library installed

--- app/(dashboard)/workflows/cards/Default.tsx
+++ app/(dashboard)/workflows/cards/Default.tsx
@@ -3,11 +3,21 @@ import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { WorkflowStatus } from "@/types/workflow";
 import { Workflow } from "@prisma/client";
-import { FileTextIcon, PlayIcon, ShuffleIcon } from "lucide-react";
+import {
+  CoinsIcon,
+  CornerDownRight,
+  FileTextIcon,
+  MoveRightIcon,
+  PlayIcon,
+  ShuffleIcon,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import WorkflowActions from "../components/WorkflowActions";
 import RunButton from "../components/RunButton";
+import SchedulerDialog from "../components/SchedulerDialog";
+import TooltipWrapper from "@/components/TooltipWrapper";
+import { Badge } from "@/components/ui/badge";
 
 interface DefaultProps {
   workflow: Workflow;
@@ -36,17 +46,25 @@ const Default = ({ workflow }: DefaultProps) => {
               <PlayIcon className="h-4 w-4 text-white" />
             )}
           </div>
-          <div className="text-base font-bold flex items-center text-muted-foreground">
-            <Link
-              href={`/workflow/editor/${workflow.id}`}
-              className="flex items-center hover:underline transition-shadow"
-            >
-              {workflow.name}
-            </Link>
-            {isDraft && (
-              <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full">
-                Draft
-              </span>
+          <div>
+            <h3 className="text-base font-bold flex items-center text-muted-foreground">
+              <Link
+                href={`/workflow/editor/${workflow.id}`}
+                className="flex items-center hover:underline transition-shadow"
+              >
+                {workflow.name}
+              </Link>
+              {isDraft && (
+                <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full">
+                  Draft
+                </span>
+              )}
+            </h3>
+            {workflow.status !== WorkflowStatus.DRAFT && (
+              <ScheduleSection
+                workflowId={workflow.id}
+                creditsCost={workflow.creditsCost}
+              />
             )}
           </div>
         </div>
@@ -73,5 +91,32 @@ const Default = ({ workflow }: DefaultProps) => {
     </Card>
   );
 };
+
+function ScheduleSection({
+  workflowId,
+  creditsCost,
+}: {
+  workflowId: string;
+  creditsCost: number;
+}) {
+  return (
+    <div className="flex items-center gap-2">
+      <CornerDownRight className="w-4 h-4 text-muted-foreground" />
+      <SchedulerDialog workflowId={workflowId} />
+      <MoveRightIcon className="h-4 w-4 text-muted-foreground" />
+      <TooltipWrapper content="Credit consumption for full run">
+        <div className="flex items-center gap-3">
+          <Badge
+            variant="outline"
+            className="space-x-2 text-muted-foreground rounded-sm"
+          >
+            <CoinsIcon className="w-4 h-4" />
+            <span>Credits cost: {creditsCost}</span>
+          </Badge>
+        </div>
+      </TooltipWrapper>
+    </div>
+  );
+}
 
 export default Default;
---
+++ app/(dashboard)/workflows/components/SchedulerDialog.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+function SchedulerDialog({ workflowId }: { workflowId: string }) {
+  return <div>SchedulerDialog</div>;
+}
+
+export default SchedulerDialog;
